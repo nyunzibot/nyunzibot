37d7dcc94b14f0fc0126b9956b324754a590e592
rohanC-dev <rohan.chopra241@gmail.com>
2025-12-16 22:22:08 -0500

    logging

--- bot.py
+++ bot.py
@@ -521,6 +521,7 @@ async def fetch_image_gelbooru(tags: str, avoid_md5s: set[str]) -> tuple[str, st
                     ) as resp:
                         http_status = resp.status
                         log.info("[GEL FETCH] tier=%s limit=%s pid<=%s status=%s", tier_label, limit, pid_max, http_status)
+                        log.info("[HTTP URL] %s", str(resp.url))
 
                         if http_status == 429:
                             await asyncio.sleep(backoffs[1])
@@ -620,6 +621,7 @@ async def fetch_image_rule34(tags: str, avoid_md5s: set[str]) -> tuple[str, str 
                     ) as resp:
                         http_status = resp.status
                         log.info("[R34 FETCH] tier=%s limit=%s pid<=%s status=%s", tier_label, limit, pid_max, http_status)
+                        log.info("[HTTP URL] %s", str(resp.url))
 
                         if http_status == 429:
                             await asyncio.sleep(backoffs[1])
