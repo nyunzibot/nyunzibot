352b15df1e97e593792bd1de09862feb0a25eb5a
rohanC-dev <rohan.chopra241@gmail.com>
2025-12-24 23:30:04 -0500

    revert

--- fetch/fetch_image.py
+++ fetch/fetch_image.py
@@ -11,17 +11,17 @@ from .danbooru import fetch_image_danbooru
 # WRAPPER: Gelbooru -> Rule34 -> Safe -> Kona -> Yande -> Dan
 # =========================
 async def fetch_image(tags: str, avoid_md5s: set[str]) -> tuple[str, str | None, str] | None:
-    # res = await fetch_image_gelbooru(tags, avoid_md5s)
-    # if res: return res
-    
-    # res = await fetch_image_rule34(tags, avoid_md5s)
-    # if res: return res
-
-    # res = await fetch_image_rule34us(tags, avoid_md5s)
-    # if res: return res
-
-    res = await fetch_image_rule34video(tags, avoid_md5s)
+    res = await fetch_image_gelbooru(tags, avoid_md5s)
     if res: return res
+    
+    res = await fetch_image_rule34(tags, avoid_md5s)
+    if res: return res
+
+    res = await fetch_image_rule34us(tags, avoid_md5s)
+    if res: return res
+
+    # res = await fetch_image_rule34video(tags, avoid_md5s)
+    # if res: return res
     
     res = await fetch_image_safebooru(tags, avoid_md5s)
     if res: return res
